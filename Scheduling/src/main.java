9211f1d82a60f9e741d48c4e5b9d5fbcc7c335e5
lapah <lapah@DESKTOP-UUVS66B>
2020-05-02 16:14:21 +0900

    //

--- Scheduling/src/main.java
+++ Scheduling/src/main.java
@@ -1,11 +1,36 @@
+import java.util.Scanner;
+
 import javax.swing.JFrame;
 
 import com.func.*;
 import com.pro.Process;
-public class Main extends JFrame{
+public class main extends JFrame{
 
-	public static void Main(String[] args) {
-//		Temp f = new Temp(1);
+	public static void main(String[] args) {
+		int arrTime, burTime;
+	      Scanner in = new Scanner(System.in);
+	      int n = in.nextInt();
+	      
+//	      CORONA crn = new CORONA(n);
+//	      for(int i = 0; i < 5;i++) {
+//	         arrTime = in.nextInt();
+//	         burTime = in.nextInt();
+//	          crn.insertPcs(arrTime, burTime);
+//	      }
+//	      //sr.doScheduling();
+//	      crn.doScheduling();
+	      
+	      
+	      //FCFS crn = new FCFS(n);
+	     CORONA crn = new CORONA(n);
+	      for(int i = 0; i < 5;i++) {
+	         arrTime = in.nextInt();
+	         burTime = in.nextInt();
+	          crn.insertPcs(arrTime, burTime);
+	      }
+	      //sr.doScheduling();
+	      crn.doScheduling();
+//		Temp f = new (1);
 //		f.insertPcs(0, 3);
 //		f.insertPcs(1, 10);
 //		f.insertPcs(0, 1);
